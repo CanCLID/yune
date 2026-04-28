b3d085e2dd3fd17ff6a3dfac545a75ea8c556104
huangjunxin <huang-junxin@qq.com>
2026-04-28 17:19:18 +0800

    Split yune-core compiled dictionary helpers

---
+++ crates/yune-core/src/dictionary/mod.rs
@@ -0,0 +1,8 @@
+mod compiled;
+
+pub use compiled::{
+    parse_rime_prism_bin_metadata, parse_rime_reverse_bin_metadata, parse_rime_table_bin_metadata,
+    rime_checksum_bytes, rime_dict_rebuild_plan, rime_dict_source_checksum, RimeChecksumComputer,
+    RimeCompiledMetadataError, RimeDictRebuildError, RimeDictRebuildInput, RimeDictRebuildPlan,
+    RimePrismBinMetadata, RimePrismChecksumMetadata, RimeReverseBinMetadata, RimeTableBinMetadata,
+};
